7c3be45254b3b837740a258ba49b708852090bcc
Francois Andrieux <francois.andrieux@gmail.com>
2021-10-16 16:32:44 -0400

    - Migrated process loop implementation to ft_render_frame_lib

--- src/procloop/process_loop.cpp
+++
@@ -1,9 +0,0 @@
-#include "process_loop.h"
-#include "process_loop_impl.h"
-
-// Execute this process main loop
-// Returns a platform specific return code
-int ft::os::procloop::process_loop::execute()
-{
-    return process_loop_impl::execute();
-}
